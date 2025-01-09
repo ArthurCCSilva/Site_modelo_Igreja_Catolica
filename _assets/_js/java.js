97f4c9ca4ac82571abdb24483420c4fc26d0cee7
ArthurCCSilva <arthurccs2013@gmail.com>
2025-01-09 17:03:24 -0300

    colocando site
    colocando site

---
+++ _assets/_js/java.js
@@ -0,0 +1,10 @@
+function showSiderbar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+
+function hideSiderbar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}
+
